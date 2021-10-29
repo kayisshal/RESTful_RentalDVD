7edef0717491606ffcc7049d5d35aaae1f928a30
kayisshal <61397531+kayisshal@users.noreply.github.com>
2021-10-29 14:56:09 +0700

    Endpoint Finish

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.get("/", async (req, res, next) => {
     res.send("Welcome to RESTful API Application.")
 });
 
+// CREATE data (insert)
 app.post("/api/actor", async (req, res, next) => {
 	try{
 		const actor = req.body
@@ -24,7 +25,9 @@ app.post("/api/actor", async (req, res, next) => {
 		next(error)
 	}
 });
-// get route
+
+
+// READ data (all data)
 app.get("/api/actor", async (req, res, next) => {
 	try{
 		const sqlText = `SELECT * FROM actor`
@@ -37,6 +40,7 @@ app.get("/api/actor", async (req, res, next) => {
 	}
 });
 
+// READ data (by ID)
 app.get("/api/actor/:id", async (req, res, next) => {
 	try{
 		const sqlText = `SELECT * FROM actor WHERE actor_id = $1`
@@ -49,8 +53,50 @@ app.get("/api/actor/:id", async (req, res, next) => {
 		next(error)
 	}
 });
-// set port, listen for requests
 
+// UPDATE data (by ID)
+app.put("/api/actor/:id", async (req, res, next) => {
+	try{
+		const actor = req.body
+		const sqlText = `UPDATE actor SET first_name = $1, last_name = $2, last_update = NOW() WHERE actor_id = $3 RETURNING *`
+		const sqlParams = [actor.first_name, actor.last_name, req.params.id]
+		const result = (await db.query(sqlText, sqlParams)).rows
+		res.json(result)
+	}
+	catch(error){
+		console.error(error)
+		next(error)
+	}
+});
+
+// DELETE data (by ID)
+app.delete("/api/actor/:id", async (req, res, next) => {
+	try{
+		const sqlText = `DELETE FROM actor WHERE actor_id = $1 RETURNING *`
+		const sqlParams = [req.params.id]
+		const result = (await db.query(sqlText, sqlParams)).rows
+		res.json(result)
+	}
+	catch(error){
+		console.error(error)
+		next(error)
+	}
+});
+
+// DELETE All data (CASCADE)
+app.delete("/api/actor", async (req, res, next) => {
+	try{
+		const sqlText = `TRUNCATE actor CASCADE`
+		const result = (await db.query(sqlText)).rows
+		res.json(result)
+	}
+	catch(error){
+		console.error(error)
+		next(error)
+	}
+});
+
+// set port, listen for requests
 app.listen(PORT, async() => {
 	try{
 		await createActorTable()
