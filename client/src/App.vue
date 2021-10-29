49aa4ea9651477d85522be0a1a786258067ddf16
kayisshal <61397531+kayisshal@users.noreply.github.com>
2021-10-29 17:32:07 +0700

    Add Vuetify + Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <Navbar title="Actor"/>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Navbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+
+    <!-- app bar / menu bar / nav bar -->
+    <v-app-bar
+      app
+    >
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+             
+      <v-spacer></v-spacer>
+
+      <!-- switch theme -->
+      <v-switch
+        :value="darkMode" 
+        @change="toggleDarkMode" 
+        :label="`${switchLabel}`"
+        hide-details
+      ></v-switch>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+    <!-- list menu -->
+    <v-list 
+      nav
+      class="mt-2"
+    >
+        <v-list-item v-for="(item, index) in items" :key="index" :to="item.route">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+                        
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+    </v-list>
+
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Navbar',
+    props:{
+        title:String,
+    },
+    data(){ 
+      return{
+        darkMode: false,
+        drawer: false,
+        items: [
+          { title: 'List Actor', icon: 'mdi-format-list-bulleted-square', route: '/' },
+          { title: 'New Actor', icon: 'mdi-account-plus', route: '/actor'},
+        ],
+      }
+    },
+    methods: {
+      toggleDarkMode: function () {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        this.darkMode = !this.darkMode;
+      }
+    },
+    computed: {
+      switchLabel: function () {
+        return this.darkMode ? 'Light' : 'Dark';
+      }
+    },
+    watch:{
+      group(){
+        this.drawer = false
+      }
+    }
+}
+</script>
