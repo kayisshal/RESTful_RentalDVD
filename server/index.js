df7762fa6c59672277d6fbb40fb005baadafdae4
kayisshal <61397531+kayisshal@users.noreply.github.com>
2021-10-29 14:01:37 +0700

    Add Endpoint 'POST'

--- server/index.js
+++ server/index.js
@@ -4,15 +4,31 @@ const PORT = 8080;
 const createActorTable = require('./db/actor')
 const db = require('./db/index')
 
+app.use(express.json())
+
 // simple route
 app.get("/", async (req, res, next) => {
     res.send("Welcome to RESTful API Application.")
 });
 
+app.post("/api/actor", async (req, res, next) => {
+	try{
+		const actor = req.body
+		const sqlText = `INSERT INTO actor (first_name, last_name, last_update) VALUES ($1, $2, NOW()) RETURNING *`
+		const sqlParams = [actor.first_name, actor.last_name]
+		const result = (await db.query(sqlText, sqlParams)).rows
+		res.json(result)
+	}
+	catch(error){
+		console.error(error)
+		next(error)
+	}
+});
+// get route
 app.get("/api/actor", async (req, res, next) => {
 	try{
-		let sqlText = `SELECT * FROM actor`
-		let result = (await db.query(sqlText)).rows
+		const sqlText = `SELECT * FROM actor`
+		const result = (await db.query(sqlText)).rows
 		res.json(result)
 	}
 	catch(error){
